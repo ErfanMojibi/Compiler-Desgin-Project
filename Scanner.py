6cc4934a7fbf8aa90bf7d6af246e0041e498cd38
erfan <mojibierfan@gmail.com>
2022-12-02 16:17:33 +0330

    small change

--- Scanner.py
+++ Scanner.py
@@ -1,12 +1,13 @@
-from DFA import DFA
+from DFA import dfa
 from SymbolTable import SymbolTable
+
 
 class Scanner:
 
-    def __init__(self, input_file):
+    def __init__(self, input_file, scanner_dfa):
         self.file = open(input_file)
         self.reach_end_of_file = False
-        self.dfa = DFA()
+        self.dfa = scanner_dfa
         self.look_ahead = None
         self.line_no = 1
         self.symbol_table = SymbolTable()
@@ -15,7 +16,7 @@ class Scanner:
 
     def get_look_ahead(self):
         return self.look_ahead
-    
+
     def move_look_ahead(self):
         if not self.reach_end_of_file:
             return
@@ -23,11 +24,11 @@ class Scanner:
             self.look_ahead = ''
             if self.look_ahead == '\n':
                 self.line_no += 1
-    
+
     def get_line_number(self):
         return self.line_no
-    
-    def get_next_token(self):    
+
+    def get_next_token(self):
         self.dfa.reset()
         buffer = ''
         while not self.dfa.is_finished():
@@ -37,13 +38,16 @@ class Scanner:
         if self.dfa.get_token_type() == 'id_key':
             if self.symbol_table.get_string_token_type(buffer) == 'ID':
                 self.symbol_table.insert_id(buffer)
-            return (self.symbol_table.get_string_token_type(buffer) ,buffer)
+            return (self.symbol_table.get_string_token_type(buffer), buffer)
         else:
             if self.dfa.error == True:
                 self.errors.append((self.line_no, self.dfa.error_message))
                 while self.look_ahead not in self.panic_chars:
                     self.move_look_ahead()
             else:
-                if self.dfa.current_state in ['long_comment_2', 'line_comment_2' ,'d_equ_symbol', 'symbol']:
+                if self.dfa.current_state in ['long_comment_2', 'line_comment_2', 'd_equ_symbol', 'symbol']:
                     self.move_look_ahead()
-                return (self.dfa.get_token_type(), buffer)    
+                return (self.dfa.get_token_type(), buffer)
+
+
+scanner = Scanner("input.txt", dfa)
