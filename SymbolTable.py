d0df0912983218a2cc0f429243bcdb7b46af4f3b
alirezahaqi <ali123haghi@gmail.com>
2022-12-02 18:39:35 +0330

    Update SymbolTable.py

--- SymbolTable.py
+++ SymbolTable.py
@@ -4,6 +4,8 @@ class SymbolTable:
     def __init__(self) -> None:
         self.keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'endif']
         self.id = []
+        self.line_dict = dict()
+
 
     def get_string_token_type(self, string):
         if string in self.keywords:
@@ -21,3 +23,12 @@ class SymbolTable:
         if string not in self.keywords and string not in self.id:
             self.id.append(string)
     
+    def export_symbol_table(self):
+        i = 1
+        out_str = ''
+        for string in self.id:
+            out_str += str(i) + '.\t' + string + '\n'
+            i += 1
+        f_out = open('symbol_table.txt' ,'w')
+        f_out.write(out_str)
+        f_out.close()
