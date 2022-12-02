78d5272b5a63110e341c884d49970a5072b4d6d3
alirezahaqi <ali123haghi@gmail.com>
2022-12-02 14:53:25 +0330

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -60,10 +60,10 @@ symbols = {';', ':', '(', ')', '[', ']', '{', '}', '+', '-', '*', '/', '<', '=='
 white_space = {' ', '\n', '\t', '\r', '\f', '\v'}
 extra_symbols = {'#'}
 
-keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'endif']
+
 alphabets = digits.union(letters, symbols, white_space)
 
-all = {i for i in range(256)}
+all = {chr(i) for i in range(256)}
 
 dfa_states_reminder = {
     's': 0,
@@ -71,6 +71,7 @@ dfa_states_reminder = {
     'num_ac': 2,
     'white_space': 16,
     'id_key': 3,
+    'division' : 50,
     'id_key_ac': 4,
 
     'white_space_ac': 21,
@@ -94,8 +95,8 @@ dfa_states_reminder = {
 }
 transition_function = [
     Transition('s', 'num', digits),
-    Transition('s', 'id_key',  letters.union(digits)),
-    Transition('num', 'num', digits),
+    Transition('s', 'id_key',  letters),
+    
 
     Transition('s', 'white_space', white_space),
     Transition('white_space', 'white_space', white_space),
@@ -104,6 +105,7 @@ transition_function = [
     Transition('id_key', 'id_key', letters.union(digits)),
     Transition('id_key', 'ik_key_ac', all - letters.union(digits)),
 
+    Transition('num', 'num', digits),
     Transition('num', 'num_ac', symbols.union(white_space)),
     Transition('num', 'num_ac', all - white_space.union(symbols), error=True, error_message='Invalid number'),
 
@@ -116,6 +118,7 @@ transition_function = [
     Transition('star', 'star_ac', all - {'\\'}),
 
     Transition('s', 'comment', {'/'}),
+    Transition('comment', 'division', all - {'/'}),
     Transition('comment', 'line_comment', {'/'}),
     Transition('line_comment', 'line_comment', all - {'\n', 5}),
     Transition('line_comment', 'line_comment_2', {5, '\n'}),
@@ -133,8 +136,9 @@ transition_function = [
     Transition('s', 'symbol', symbols - {'=', '/'})
 ]
 
-accept_states = ['unclosed_comment_2', 'long_comment_2', 'unclosed_comment', 'line_comment_2', 'star_ac', 'ik_key_ac',
-                 'num_ac', 'white_space_ac', 'symbol']
+accept_states = ['unclosed_comment_2', 'long_comment_2', 'unclosed_comment', 'line_comment_2', 'star_ac', 'ik_key_ac'
+                ,'d_equ_symbol', 'num_ac', 'white_space_ac', 'symbol', 'equ_symbol_ac', 'division']
+                 
 token_types = {'long_comment_2': 'comment',
                'line_comment_2': 'comment',
                'star_ac': 'symbol',
@@ -145,5 +149,5 @@ token_types = {'long_comment_2': 'comment',
                }
 dfa = DFA(dfa_states_reminder.keys(), alphabets, transition_function, 's', accept_states, token_types)
 
-dfa.move("a")
+dfa.move("/")
 print(dfa.current_state)
