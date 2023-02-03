8e77103faab2b06a93e3ea6a25bbc3b8eec5e154
ErfanMojibi <mojibierfan@gmail.com>
2023-02-04 00:19:44 +0330

    switch handeled

--- CodeGen.py
+++ CodeGen.py
@@ -49,8 +49,10 @@ class CodeGenerator:
         for _ in range(size):
             self.program_block.append(None)
             self.semantic_stack.append(len(self.program_block) - 1)
+        print(self.semantic_stack)
 
     def jpf(self):
+        print(self.semantic_stack)
         self.program_block[self.semantic_stack[-1]] = f'(JPF, {self.semantic_stack[-2]}, {len(self.program_block)}, )'
         self.pop_from_semantic_stack(2)
 
@@ -129,7 +131,6 @@ class CodeGenerator:
         self.program_block.append(None)
 
         self.semantic_stack.append(len(self.program_block))
-        # self.semantic_stack.append(break_temp)
 
     def while_end(self):
         pc = len(self.program_block)
@@ -138,6 +139,21 @@ class CodeGenerator:
         self.program_block[self.where_to_break[-1]] = f'(ASSIGN, #{pc + 1}, {self.break_temp_address[-1]}, )'
         self.pop_from_semantic_stack(3)
 
+        self.where_to_break.pop()
+        self.break_temp_address.pop()
+
+    def switch_start(self):
+        break_temp = self.get_temp()
+        self.break_temp_address.append(break_temp)
+        # save one empty place for break
+        self.program_block.append(f'(ASSIGN, #0, {break_temp}, )')
+        self.where_to_break.append(len(self.program_block))
+        self.program_block.append(None)
+
+    def switch_end(self):
+        self.semantic_stack.pop()  # pop expression
+        self.program_block[
+            self.where_to_break[-1]] = f'(ASSIGN, #{len(self.program_block)}, {self.break_temp_address[-1]}, )'
         self.where_to_break.pop()
         self.break_temp_address.pop()
 
@@ -171,6 +187,15 @@ class CodeGenerator:
             self.pop_from_semantic_stack(2)
             self.semantic_stack.append(f'@{temp}')
 
+    def fake_save(self):
+        self.semantic_stack.append(f'#{0}')
+        self.save()
+
+    def case_condition(self, num):
+        temp = self.get_temp()
+        self.program_block.append(f'(EQ, #{num}, {self.semantic_stack[-1]}, {temp})')
+        self.semantic_stack.append(temp)
+
     def generate_code(self, token, rule_no):
         rule = self.grammar[rule_no]
         rule_rhs = rule[2:]
@@ -198,7 +223,7 @@ class CodeGenerator:
         elif rule_no == 77:  # while_condition
             self.while_condition()
         elif rule_no == 78:  # switch_start
-            pass
+            self.switch_start()
         elif rule_no == 6 or rule_no == 15 or rule_no == 16:  # declare for var_declaration and param
             self.declare()
         elif rule_no == 7:  # declare_array
@@ -207,7 +232,7 @@ class CodeGenerator:
             self.pop_exp()
         elif rule_no == 29:  # break_jp
             self.break_jp()
-        elif rule_no == 31 or rule_no == 39:  # jpf
+        elif rule_no == 31 or rule_no == 80:  # jpf
             self.jpf()
         elif rule_no == 32:  # jp
             self.jp()
@@ -225,6 +250,13 @@ class CodeGenerator:
             self.mult()
         elif rule_no == 62:  # call
             self.call()
+        elif rule_no == 79:  # fake_save : always true
+            self.fake_save()
+        elif rule_no == 36:  # switch_end
+            self.switch_end()
+        elif rule_no == 81:  # case_condition
+            print("hello")
+            self.case_condition(token[2])
 
     def export_PB(self):
         with open('output.txt', 'w') as f:
@@ -232,5 +264,3 @@ class CodeGenerator:
             for index, line in enumerate(self.program_block):
                 f.write(f'   {line}\n')
                 # print(f'{index}    {line}')
-
-    pass
